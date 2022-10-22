8ba1fb2f98fbc6cfe96933bb2bfd7ab1e68f3a8b
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-22 12:00:12 +0200

    feat: header structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,23 @@
-export function index() {
-  return <div>index</div>;
+import styles from './style.module.css';
+
+import Logo from '/logo.svg';
+import { PlusCircle } from 'phosphor-react';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src={Logo} alt="Rocket Logo" />
+        <h1>
+          to<span>do</span>
+        </h1>
+      </div>
+      <div className={styles.newTaskBar}>
+        <input type="text" />
+        <button>
+          Criar <PlusCircle size={16} />
+        </button>
+      </div>
+    </header>
+  );
 }
