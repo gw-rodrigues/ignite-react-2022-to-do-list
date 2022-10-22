7c19d4b4bfc53894fd917b7b05be5e22642e9132
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-22 12:28:22 +0200

    feat: task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 
 import { Header } from './components/Header';
+import { TaskList } from './components/TaskList';
 
 import styles from './styles/App.module.css';
 
 export function App() {
   const [count, setCount] = useState(0);
 
-  return <Header />;
+  return (
+    <div className={styles.container}>
+      <Header />
+      <TaskList />
+    </div>
+  );
 }
