4f1093085097c28b6602be880fe0af86ae030982
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-24 13:00:46 +0200

    feat: list empty and list tasks

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,15 +1,40 @@
+import { useState } from 'react';
+import { TTask } from '../../App';
 import { TaskListEmpty } from '../TaskListEmpty';
 import { TaskListHeader } from '../TaskListHeader';
 import { TaskListItems } from '../TaskListItems';
 import styles from './style.module.css';
 
-export function TaskList() {
-  const isListEmpty = false;
+interface ITaskList {
+  tasks: TTask[];
+  handleCompleteTask: (id: number) => void;
+  handleRemoveTask: (id: number) => void;
+}
+
+export function TaskList({
+  tasks,
+  handleCompleteTask,
+  handleRemoveTask,
+}: ITaskList) {
+  const isListEmpty = tasks.length === 0;
+
   return (
     <main className={styles.taskList}>
       <TaskListHeader />
-
-      {isListEmpty ? <TaskListEmpty /> : <TaskListItems />}
+      {isListEmpty ? (
+        <TaskListEmpty />
+      ) : (
+        <ul className={styles.taskListItems}>
+          {tasks.map((task) => (
+            <TaskListItems
+              key={task.id}
+              task={task}
+              handleCompleteTask={handleCompleteTask}
+              handleRemoveTask={handleRemoveTask}
+            />
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
