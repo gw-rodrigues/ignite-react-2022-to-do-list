1ade8bcb3e2029af9b0077c37523613eb31b6eb8
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-24 11:03:00 +0200

    feat: task list empty and/or items

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,9 +1,15 @@
+import { TaskListEmpty } from '../TaskListEmpty';
 import { TaskListHeader } from '../TaskListHeader';
+import { TaskListItems } from '../TaskListItems';
 import styles from './style.module.css';
+
 export function TaskList() {
+  const isListEmpty = false;
   return (
     <main className={styles.taskList}>
       <TaskListHeader />
+
+      {isListEmpty ? <TaskListEmpty /> : <TaskListItems />}
     </main>
   );
 }
