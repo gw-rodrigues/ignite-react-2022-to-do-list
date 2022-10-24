7ef9e50a1acb46911a0da2df5eab419eb7a15757
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-24 13:01:25 +0200

    feat: task list, handle create, completed, removed

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,52 @@ import { TaskList } from './components/TaskList';
 
 import styles from './styles/App.module.css';
 
+export type TTask = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+};
+
 export function App() {
-  const [count, setCount] = useState(0);
+  const [tasks, setTasks] = useState<TTask[]>([
+    {
+      id: 1,
+      title:
+        'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+      isCompleted: false,
+    },
+  ]);
+
+  function handleCreateTasks(title: string) {
+    const newTask: TTask = {
+      id: tasks.length + 1,
+      title,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+    console.log(tasks);
+  }
+
+  function handleCompleteTask(id: number) {
+    const completedTask = tasks.map((task) => {
+      if (task.id === id) {
+        task.isCompleted = !task.isCompleted;
+      }
+      return task;
+    });
+    setTasks(completedTask);
+  }
+
+  function handleRemoveTask(id: number) {}
 
   return (
     <div className={styles.container}>
-      <Header />
-      <TaskList />
+      <Header handleCreateTasks={handleCreateTasks} />
+      <TaskList
+        tasks={tasks}
+        handleCompleteTask={handleCompleteTask}
+        handleRemoveTask={handleRemoveTask}
+      />
     </div>
   );
 }
